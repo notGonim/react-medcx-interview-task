b66a9df157e069478a4d769de12c3fea88d14e48
notGonim <mhmodgonim96@gmail.com>
2021-01-28 04:49:25 +0200

    Done : with header

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,9 +1,37 @@
 import React from 'react'
 import './header.scss'
+import { Avatar, IconButton } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { auth } from '../../firebase';
+import { Logout } from '../../actions/userActions'
+
 export const Header = () => {
+
+    const { ...data } = useSelector(state => state.user)
+    const { displayName, email, photoURL } = data.user
+    const dispatch = useDispatch()
+
+    const signout = () => {
+        auth.signOut().then(() => {
+            dispatch(Logout())
+        })
+    }
+
+
+
     return (
         <div className="header">
-            
+            <div className="header_left">
+                <Link to="/">Home</Link>
+                <Link to="/post">Post</Link>
+            </div>
+            <div className="header_right">
+                <Avatar onClick={signout} src={photoURL} className="avatar" />
+            </div>
+
+
         </div>
     )
 }
+
